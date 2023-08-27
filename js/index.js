06cc191930bde739efa815a7dbd6c44707955f95
igorAlves321 <64875210+igorAlves321@users.noreply.github.com>
2023-08-26 23:21:05 -0300

    alterando título da página e corrigindo alguns bugs na lógica do código.7

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function validarEntrada(peso, altura, idade) {
     var erros = [];
     if (!peso || isNaN(peso) || peso <= 0) {
@@ -27,28 +26,35 @@ function calcularTMB(peso, altura, idade, sexo) {
            10 * peso + 6.25 * altura - 5 * idade - 161;
 }
 
+function calcularCaloriasTotais(tmb, atividade) {
+    return tmb * parseFloat(atividade);
+}
+
 function calcular() {
     var peso = parseFloat(document.getElementById("peso").value);
     var altura = parseFloat(document.getElementById("altura").value);
     var altura_m = altura / 100;
     var idade = parseFloat(document.getElementById("idade").value);
-    var sexo = document.getElementById("sexo").value;
+var sexo = document.querySelector('input[name="sexo"]:checked').value;
+    var atividade = document.getElementById("atividade").value;
 
     var erros = validarEntrada(peso, altura, idade);
     if (erros.length > 0) {
-        alert("Erros:\n" + erros.join("\n"));
+        alert("Erros:\\n" + erros.join("\\n"));
         return false;
     }
 
     var imc = calcularIMC(peso, altura_m);
     var consumo_de_agua = calcularAgua(peso);
     var tmb = calcularTMB(peso, altura, idade, sexo);
+    var calorias_totais = calcularCaloriasTotais(tmb, atividade);
 
     document.getElementById("peso_na_tabela").innerHTML = peso.toFixed(2) + " kg";
     document.getElementById("altura_na_tabela").innerHTML = altura.toFixed(2) + " cm";
     document.getElementById("imc").innerHTML = imc.toFixed(2);
     document.getElementById("consumo_de_agua").innerHTML = "Você deve tomar " + consumo_de_agua.toFixed(2) + " litros de água por dia";
     document.getElementById("tmb").innerHTML = tmb.toFixed(2) + " calorias";
+    document.getElementById("calorias_totais").innerHTML = calorias_totais.toFixed(2) + " calorias";
 }
 
 document.querySelector("form").onsubmit = function(event) {
