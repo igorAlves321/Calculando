f4d23057f6e5a387684d951bcb6db060617bbaa5
igorAlves321 <64875210+igorAlves321@users.noreply.github.com>
2023-09-05 19:17:02 -0300

    adcionando vlidações e concertando bug da tabela.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,40 @@
+// Função para definir um campo como inválido
+function setInvalidField(fieldId) {
+    document.getElementById(fieldId).classList.add("is-invalid");
+}
+
+// Função para remover a marcação de campo inválido
+function removeInvalidField(fieldId) {
+    document.getElementById(fieldId).classList.remove("is-invalid");
+}
+
+// Função para validar os dados de entrada
 function validarEntrada(peso, altura, idade) {
     var erros = [];
     if (!peso || isNaN(peso) || peso <= 0) {
         erros.push("Peso inválido");
+        setInvalidField("peso");
+    } else {
+        removeInvalidField("peso");
     }
+
     if (!altura || isNaN(altura) || altura <= 0) {
         erros.push("Altura inválida");
+        setInvalidField("altura");
+    } else {
+        removeInvalidField("altura");
     }
+
     if (!idade || isNaN(idade) || idade <= 0) {
         erros.push("Idade inválida");
+        setInvalidField("idade");
+    } else {
+        removeInvalidField("idade");
     }
     return erros;
 }
 
+// Funções para cálculos
 function calcularIMC(peso, altura_m) {
     return peso / (altura_m * altura_m);
 }
@@ -30,6 +53,7 @@ function calcularCaloriasTotais(tmb, atividade) {
     return tmb * parseFloat(atividade);
 }
 
+// Função principal para calcular e mostrar os resultados
 function calcular() {
     var peso = parseFloat(document.getElementById("peso").value);
     var altura = parseFloat(document.getElementById("altura").value);
@@ -41,35 +65,38 @@ function calcular() {
     var erros = validarEntrada(peso, altura, idade);
     if (erros.length > 0) {
         alert("Erros:\\n" + erros.join("\\n"));
-    }
-
-    var imc = calcularIMC(peso, altura_m);
-    var consumo_de_agua = calcularAgua(peso);
-    var tmb = calcularTMB(peso, altura, idade, sexo);
-    var calorias_totais = calcularCaloriasTotais(tmb, atividade);
-
-    // Classificação do IMC
-    var imc_classification = "";
-    if (imc < 18.5) {
-        imc_classification = " (Abaixo do peso)";
-    } else if (imc >= 18.5 && imc < 24.9) {
-        imc_classification = " (Normal)";
-    } else if (imc >= 25 && imc < 29.9) {
-        imc_classification = " (Sobrepeso)";
-    } else if (imc >= 30 && imc < 34.9) {
-        imc_classification = " (Obesidade I)";
-    } else if (imc >= 35 && imc < 39.9) {
-        imc_classification = " (Obesidade II)";
     } else {
-        imc_classification = " (Obesidade III)";
-    }
+        var imc = calcularIMC(peso, altura_m);
+        var consumo_de_agua = calcularAgua(peso);
+        var tmb = calcularTMB(peso, altura, idade, sexo);
+        var calorias_totais = calcularCaloriasTotais(tmb, atividade);
 
-    document.getElementById("peso_na_tabela").innerHTML = peso.toFixed(2) + " kg";
-    document.getElementById("altura_na_tabela").innerHTML = altura.toFixed(2) + " cm";
-    document.getElementById("imc").innerHTML = imc.toFixed(2) + imc_classification;
-    document.getElementById("consumo_de_agua").innerHTML = "Você deve tomar " + consumo_de_agua.toFixed(2) + " litros de água por dia";
-    document.getElementById("tmb").innerHTML = tmb.toFixed(2) + " calorias";
-    document.getElementById("calorias_totais").innerHTML = calorias_totais.toFixed(2) + " calorias";
+        // Classificação do IMC
+        var imc_classification = "";
+        if (imc < 18.5) {
+            imc_classification = " (Abaixo do peso)";
+        } else if (imc >= 18.5 && imc < 24.9) {
+            imc_classification = " (Normal)";
+        } else if (imc >= 25 && imc < 29.9) {
+            imc_classification = " (Sobrepeso)";
+        } else if (imc >= 30 && imc < 34.9) {
+            imc_classification = " (Obesidade I)";
+        } else if (imc >= 35 && imc < 39.9) {
+            imc_classification = " (Obesidade II)";
+        } else {
+            imc_classification = " (Obesidade III)";
+        }
+
+        document.getElementById("peso_na_tabela").innerHTML = peso.toFixed(2) + " kg";
+        document.getElementById("altura_na_tabela").innerHTML = altura.toFixed(2) + " cm";
+        document.getElementById("imc").innerHTML = imc.toFixed(2) + imc_classification;
+        document.getElementById("consumo_de_agua").innerHTML = "Você deve tomar " + consumo_de_agua.toFixed(2) + " litros de água por dia";
+        document.getElementById("tmb").innerHTML = tmb.toFixed(2) + " calorias";
+        document.getElementById("calorias_totais").innerHTML = calorias_totais.toFixed(2) + " calorias";
+
+        // Mostrar a tabela
+        document.getElementById("resultsTable").style.display = "table";
+    }
 
     // Limpar os campos do formulário após o cálculo ou erros
     document.getElementById("peso").value = "";
@@ -80,6 +107,7 @@ function calcular() {
     document.getElementById("atividade").value = "1.2"; // valor padrão para 'Sedentário'
 }
 
+// Evento de submissão do formulário
 document.querySelector("form").onsubmit = function(event) {
   event.preventDefault();
   calcular();
