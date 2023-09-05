35779ca2a172322f2038b207379ef9af31d1d37f
igorAlves321 <64875210+igorAlves321@users.noreply.github.com>
2023-09-05 13:56:07 -0300

    adcionando clacificação na tabela de imc, agora, quando usuário for ver seu IMC o mesmo vai saber se ele está em um peso ideal, ou não.

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ function calcular() {
     var altura = parseFloat(document.getElementById("altura").value);
     var altura_m = altura / 100;
     var idade = parseFloat(document.getElementById("idade").value);
-var sexo = document.querySelector('input[name="sexo"]:checked').value;
+    var sexo = document.querySelector('input[name="sexo"]:checked').value;
     var atividade = document.getElementById("atividade").value;
 
     var erros = validarEntrada(peso, altura, idade);
@@ -49,9 +49,25 @@ var sexo = document.querySelector('input[name="sexo"]:checked').value;
     var tmb = calcularTMB(peso, altura, idade, sexo);
     var calorias_totais = calcularCaloriasTotais(tmb, atividade);
 
+    // Classificação do IMC
+    var imc_classification = "";
+    if (imc < 18.5) {
+        imc_classification = " (Abaixo do peso)";
+    } else if (imc >= 18.5 && imc < 24.9) {
+        imc_classification = " (Normal)";
+    } else if (imc >= 25 && imc < 29.9) {
+        imc_classification = " (Sobrepeso)";
+    } else if (imc >= 30 && imc < 34.9) {
+        imc_classification = " (Obesidade I)";
+    } else if (imc >= 35 && imc < 39.9) {
+        imc_classification = " (Obesidade II)";
+    } else {
+        imc_classification = " (Obesidade III)";
+    }
+
     document.getElementById("peso_na_tabela").innerHTML = peso.toFixed(2) + " kg";
     document.getElementById("altura_na_tabela").innerHTML = altura.toFixed(2) + " cm";
-    document.getElementById("imc").innerHTML = imc.toFixed(2);
+    document.getElementById("imc").innerHTML = imc.toFixed(2) + imc_classification;
     document.getElementById("consumo_de_agua").innerHTML = "Você deve tomar " + consumo_de_agua.toFixed(2) + " litros de água por dia";
     document.getElementById("tmb").innerHTML = tmb.toFixed(2) + " calorias";
     document.getElementById("calorias_totais").innerHTML = calorias_totais.toFixed(2) + " calorias";
